466046d7c20a1d65f8864c65020a15b88b4cc17a
CKanishka <ckanishka24@gmail.com>
2020-01-15 18:21:46 +0530

    Updates

---
+++ precache-manifest.06f7d5070e14c57bf450363037e73945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4efa9eee47421695161e2880be47c48",
+    "url": "/grab-issues/index.html"
+  },
+  {
+    "revision": "cfe6880fe6ec1ffc0f78",
+    "url": "/grab-issues/static/css/main.7fabbeec.chunk.css"
+  },
+  {
+    "revision": "4ef031b06569ea9cea00",
+    "url": "/grab-issues/static/js/2.067aa0c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/grab-issues/static/js/2.067aa0c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfe6880fe6ec1ffc0f78",
+    "url": "/grab-issues/static/js/main.711b4b39.chunk.js"
+  },
+  {
+    "revision": "d7678bce340220115928",
+    "url": "/grab-issues/static/js/runtime-main.4f750ff4.js"
+  }
+]);
